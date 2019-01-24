9d5dced44a0a99b4204019b7e0fb9abb7de7a60f
Sean Sullivan <seans@google.com>
2019-01-24 11:17:29 -0800

    Added documentation to main()

--- cmd/kubectl/kubectl.go
+++ cmd/kubectl/kubectl.go
@@ -32,6 +32,17 @@ import (
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 )
 
+// The kubectl dispatcher is a wrapper which retrieves the server version from
+// a cluster, and executes the appropriate kubectl version. For example, if a
+// user is configured to talk to their Kubernetes cluster that is version
+// 1.10.3-gke, then this binary will execute "kubectl-1.10" (in the same
+// directory as this binary) passing the arguments and environment of
+// this binary.
+//
+// IMPORTANT: Versioned kubectl binaries that are dispatched to, MUST be in
+// the same directory as this dispatcher binary. Versioned kubectl binaries
+// MUST follow the naming convention: kubectl-<major>.<minor>. Example:
+// kubectl-1.12.
 func main() {
 	// Create a defensive copy of the args and the environment.
 	args := make([]string, len(os.Args))
