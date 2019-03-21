4d8df3b9954e8f63a363bccd4a283d10d636ba1a
Sean Sullivan <seans@google.com>
2019-03-21 14:36:50 -0700

    Add LICENCE, CONTRIBUTING and updated license for bash scripts

--- tests/clean.sh
+++ tests/clean.sh
@@ -1,10 +1,25 @@
+##################################################################
+# Copyright 2019 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+##################################################################
+
 echo "Cleaning; make clean"
 make clean
 echo
 
-echo "Recreating directory: /var/run/kubernetes"
-sudo mkdir -p /var/run/kubernetes
-sudo chmod 777 /var/run/kubernetes
+# NOTE: You may have to recreate dir: /var/run/kubernetes
 
 DISPATCHER_BIN="${HOME}/go/src/github.com/kubectl-dispatcher"
 KUBECTL_BIN="_output/local/bin/linux/amd64"
--- tools/dispatcher-integrate.sh
+++ tools/dispatcher-integrate.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
-#############################################################
+##################################################################
+# Copyright 2019 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 # Tool to help integrate dispatcher source code into
 # a version of kubectl source code. This will update most
@@ -7,7 +20,7 @@
 # as well. This script was first used to integrate kubectl
 # version 1.11.7.
 #
-#############################################################
+##################################################################
 
 DISPATCHER_ORIGIN=${HOME}/go/src/github.com/kubectl-dispatcher/pkg
 KUBECTL_DESTINATION=${HOME}/go/src/k8s.io/kubernetes/pkg/kubectl/dispatcher
